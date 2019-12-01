2e70ae0ed348762a0c3a238ac375d209169204ca
nathaliekng <41026746+nathaliekng@users.noreply.github.com>
2019-12-01 12:51:48 -0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br><br><br><h1>Welcome to the home page of Nathalie, Brandon and Sameer's CPS530 final project.</h1>
     <img src="w-dogs.jpg" alt="" width=100% height=25%>
-     <br><h1> Browse through the pages using the icon on the top left corner of this page!</h1>
+     <br><h1> Browse through the pages using the navigation bar by clicking the icon on the top left corner of this page! Submit feedback by clicking the feedback button on the top right corner.</h1>
   </div>
 </template>
 
